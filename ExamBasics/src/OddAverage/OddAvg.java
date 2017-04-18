a13cbd61d0bf385012f1961788fd16aa9d6c35cb
georgezsiga <georgezsiga@gmail.com>
2017-04-18 10:38:52 +0200

    add averageofoddnumbers method

--- ExamBasics/src/OddAverage/OddAvg.java
+++ ExamBasics/src/OddAverage/OddAvg.java
@@ -5,6 +5,20 @@ package OddAverage;
  */
 
 public class OddAvg {
+
+  public double averageValueOfOddNumbers(int[] numbers) {
+    int totalOfOddNumbers = 0;
+    int counter = 0;
+    for (int i = 1; i < numbers.length; i += 2) {
+      if (numbers[i] % 2 == 1) {
+        totalOfOddNumbers += i;
+        counter ++;
+      }
+    }
+    double averageOfOddNumbers = totalOfOddNumbers / counter;
+    return averageOfOddNumbers;
+  }
+
   public static void main(String[] args) {
     // Create a function called `oddAverage` that takes a list of numbers as parameter
     // and returns the average value of the odd numbers in the list
