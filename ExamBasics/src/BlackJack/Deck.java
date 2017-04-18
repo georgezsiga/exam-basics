4405584d8ec2b109b12eb7c0dc0c9ad39a89eba2
georgezsiga <georgezsiga@gmail.com>
2017-04-18 11:48:03 +0200

    Add the method to count colours in the deck

--- ExamBasics/src/BlackJack/Deck.java
+++ ExamBasics/src/BlackJack/Deck.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class Deck {
 
   ArrayList<Card> deck;
-  int numberOfCards;
+  int numberOfCards, hearts, spades, diamonds, clubs;
 
   public Deck(int wholeNumber) {
     deck = new ArrayList<Card>();
@@ -18,7 +18,7 @@ public class Deck {
 
   public void addCards() {
     if (numberOfCards < 4) {
-      for (int i = 0; i < numberOfCards ; i++) {
+      for (int i = 0; i < numberOfCards; i++) {
         Card card = new Card(i, randomCardValue());
         deck.add(card);
       }
@@ -28,15 +28,35 @@ public class Deck {
           Card card = new Card(i, randomCardValue());
           deck.add(card);
         } else {
-        Card card = new Card(randomCardColor(), randomCardValue());
-        deck.add(card);
+          Card card = new Card(randomCardColor(), randomCardValue());
+          deck.add(card);
+        }
       }
     }
   }
 
+  public void colorsInTheDeck() {
+    hearts = 0;
+    spades = 0;
+    diamonds = 0;
+    clubs = 0;
+    for (int i = 0; i < deck.size() ; i++) {
+      if (deck.get(i).color.contains("Hearts")) {
+        hearts ++;
+      } else if (deck.get(i).color.contains("Spades")) {
+        spades ++;
+      } else if (deck.get(i).color.contains("Diamonds")) {
+        diamonds ++;
+      } else {
+        clubs ++;
+      }
+    }
+  }
+
+
   @Override
   public String toString() {
-    return numberOfCards + " cards - ";
+    return numberOfCards + " cards - " + clubs + " Clubs, " + diamonds + " Diamonds, " + hearts + " Hearts, " + spades + " Spades";
   }
 
   public int randomCardValue() {
