362e1d5806af040afda31f723718df317d3961e5
georgezsiga <georgezsiga@gmail.com>
2017-04-18 11:04:11 +0200

    Add readfrom file and print to file methods

--- ExamBasics/src/Copy.java
+++ ExamBasics/src/Copy.java
@@ -1,3 +1,9 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 /**
  * Created by georgezsiga on 4/18/17.
  */
@@ -37,15 +43,39 @@ public class Copy {
     System.out.println("No destination provided");
   }
 
+  private void copyFile() {
+
+    String fileSource;
+    String destination;
+    try {
+      Path filePath = Paths.get(fileSource);
+      List<String> lines = Files.readAllLines(filePath);
+      printToFile(destination, lines);
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.out.println("There is a prbblem reading the file");
+    }
+  }
+
+  public void printToFile(String destination, List<String> content) {
+    try {
+      Path filePath = Paths.get(destination);
+      Files.write(filePath, content);
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.out.println("Uh-oh, could not write the file!");
+    }
+    System.out.println("You successfully copied the file");
+  }
+
   public static void main(String[] args) {
     Copy argument = new Copy(args);
     if (args.length == 0) {
       argument.noArgument();
     } else if (args.length == 1) {
       argument.oneArgument();
-
+    } else {
+      argument.copyFile();
     }
-
-
   }
 }
