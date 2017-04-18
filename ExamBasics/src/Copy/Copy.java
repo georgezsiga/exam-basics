a329fba1ef42d14bf7ef719f374f62bbe74a95d4
georgezsiga <georgezsiga@gmail.com>
2017-04-18 09:43:49 +0200

    Add constructor

--- ExamBasics/src/Copy/Copy.java
+++ ExamBasics/src/Copy/Copy.java
@@ -3,16 +3,24 @@ package Copy;
 /**
  * Created by georgezsiga on 4/18/17.
  */
-
+// This should be the basic replica of the 'cp' command
+// If ran from the command line without arguments
+// It should print out the usage:
+// copy [source] [destination]
+// When just one argument is provided print out
+// No destination provided
+// When both arguments provided and the source is a file
+// Read all contents from it and write it to the destination
 public class Copy {
+
+  private String[] args;
+
+  private Copy(String[] args) {
+    this.args = args;
+  }
   public static void main(String[] args) {
-    // This should be the basic replica of the 'cp' command
-    // If ran from the command line without arguments
-    // It should print out the usage:
-    // copy [source] [destination]
-    // When just one argument is provided print out
-    // No destination provided
-    // When both arguments provided and the source is a file
-    // Read all contents from it and write it to the destination
+
+
+
   }
 }
