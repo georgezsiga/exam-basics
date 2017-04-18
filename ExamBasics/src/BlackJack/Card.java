c78b9ae6709a8136907de25cc5836cb43719a1a9
georgezsiga <georgezsiga@gmail.com>
2017-04-18 09:56:57 +0200

    Set color and value fields and add constructor

--- ExamBasics/src/BlackJack/Card.java
+++ ExamBasics/src/BlackJack/Card.java
@@ -4,5 +4,11 @@ package BlackJack;
  * Created by georgezsiga on 4/18/17.
  */
 public class Card {
+  String color;
+  int value;
 
+  public Card(String color, int value) {
+    this.color = color;
+    this.value = value;
+  }
 }
