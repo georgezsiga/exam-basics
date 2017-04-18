5b4685a79591793f56c79d505933d79fc8fa8347
georgezsiga <georgezsiga@gmail.com>
2017-04-18 12:37:08 +0200

    Add initial test file

---
+++ ExamBasics/src/OddAverage/Test/OddAverage/OddAvgTest.java
@@ -0,0 +1,18 @@
+package OddAverage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by georgezsiga on 4/18/17.
+ */
+class OddAvgTest {
+
+  @org.junit.jupiter.api.Test
+  void averageValueOfOddNumbers() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void main() {
+  }
+
+}
