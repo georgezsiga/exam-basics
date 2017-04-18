2496bf029d6a389f2176430563c2e94044b8fccd
georgezsiga <georgezsiga@gmail.com>
2017-04-18 09:30:40 +0200

    Add initial file

---
+++ ExamBasics/src/OddAverage/OddAvg.java
@@ -0,0 +1,13 @@
+package OddAverage;
+
+/**
+ * Created by georgezsiga on 4/18/17.
+ */
+
+public class OddAvg {
+  public static void main(String[] args) {
+    // Create a function called `oddAverage` that takes a list of numbers as parameter
+    // and returns the average value of the odd numbers in the list
+    // Create basic unit tests for it with at least 3 different test cases
+  }
+}
