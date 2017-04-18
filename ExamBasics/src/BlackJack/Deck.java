9553aef91f48b0dae6dde7d9b3e16941f868e112
georgezsiga <georgezsiga@gmail.com>
2017-04-18 10:02:44 +0200

    Add deck and numberofcards fields and constructor

--- ExamBasics/src/BlackJack/Deck.java
+++ ExamBasics/src/BlackJack/Deck.java
@@ -1,8 +1,19 @@
 package BlackJack;
 
+import java.util.ArrayList;
+
 /**
  * Created by georgezsiga on 4/18/17.
  */
 public class Deck {
+
+  ArrayList<Card> deck;
+  int numberOfCards;
+
+  public Deck(int numberOfCards) {
+    deck = new ArrayList<Card>();
+    this.numberOfCards = numberOfCards;
+  }
+
 
 }
