7f5dc7a681bb3e8fcd68d06d776034d199911417
georgezsiga <georgezsiga@gmail.com>
2017-04-18 10:07:57 +0200

    Add the required number of cards to the deck

--- ExamBasics/src/BlackJack/Deck.java
+++ ExamBasics/src/BlackJack/Deck.java
@@ -10,10 +10,17 @@ public class Deck {
   ArrayList<Card> deck;
   int numberOfCards;
 
-  public Deck(int numberOfCards) {
+  public Deck(int wholeNumber) {
     deck = new ArrayList<Card>();
-    this.numberOfCards = numberOfCards;
+    this.numberOfCards = wholeNumber;
+    addCards();
   }
 
+  public void addCards() {
+    for (int i = 0; i < numberOfCards ; i++) {
+      Card card = new Card("Spade", "2");
+      deck.add(card);
+    }
+  }
 
 }
