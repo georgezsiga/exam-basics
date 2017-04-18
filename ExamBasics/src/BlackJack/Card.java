7cdb8693c129b5df9b18e1b38ff22ff23ffc5967
georgezsiga <georgezsiga@gmail.com>
2017-04-18 11:26:49 +0200

    Converts random card number and color values to cards and colors

--- ExamBasics/src/BlackJack/Card.java
+++ ExamBasics/src/BlackJack/Card.java
@@ -7,9 +7,27 @@ public class Card {
 
   String color, value;
 
-  public Card(String color, String value) {
-    this.color = color;
-    this.value = value;
+  public Card(int randColor, int randNum) {
+    if (randColor == 0) {
+      this.color = "Hearts";
+    } else if (randColor == 1) {
+      this.color = "Spades";
+    } else if (randColor == 2) {
+      this.color = "Diamonds";
+    } else {
+      this.color = "Clubs";
+    }
+    if (randNum == 1) {
+      this.value = "Ace";
+    } else if (randNum > 0 && randNum < 11) {
+      this.value = Integer.toString(randNum);
+    } else if (randNum == 11) {
+      this.value = "Jack";
+    } else if (randNum == 12) {
+      this.value = "Queen";
+    } else {
+      this.value = "King";
+    }
   }
 
   @Override
