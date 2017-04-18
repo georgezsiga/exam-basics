d2c2ec3302bc0e65eec6ea7ee967ffd8f71783f7
georgezsiga <georgezsiga@gmail.com>
2017-04-18 09:58:39 +0200

    Add toString method and change value to string

--- ExamBasics/src/BlackJack/Card.java
+++ ExamBasics/src/BlackJack/Card.java
@@ -4,11 +4,15 @@ package BlackJack;
  * Created by georgezsiga on 4/18/17.
  */
 public class Card {
-  String color;
-  int value;
+  String color, value;
 
-  public Card(String color, int value) {
+  public Card(String color, String value) {
     this.color = color;
     this.value = value;
+  }
+
+  @Override
+  public String toString() {
+    return value + " " +  color;
   }
 }
